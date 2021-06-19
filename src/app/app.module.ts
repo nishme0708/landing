fcb227660304b1bf788f644d722589be8b412707
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2021-06-19 20:38:09 +0530

    Import notifications module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotificationsModule } from './notifications/notifications.module';
 import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, WeatherModule ],
+    imports: [ BrowserModule, WeatherModule, NotificationsModule ],
     providers: [],
     bootstrap: [ AppComponent ]
 })
