52d2c74bb6bc300709c16508406ce6759e39cc54
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2021-06-20 11:28:11 +0530

    Add imports to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewsApiModule } from './news-api/news-api.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
     declarations: [ AppComponent ],
-    imports: [ BrowserModule, WeatherModule, NotificationsModule ],
+    imports: [ BrowserModule, WeatherModule, NotificationsModule, NewsApiModule ],
     providers: [],
     bootstrap: [ AppComponent ]
 })
