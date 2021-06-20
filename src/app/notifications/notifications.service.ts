32d0c7b7175737a3b48c27aadde4b8c359711baa
Nishant Mehta <42231185+nishme0708@users.noreply.github.com>
2021-06-20 11:27:37 +0530

    Add notification service changes

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { scan } from 'rxjs/operators';
 
+export interface Command {
+    message?: string;
+    type: 'success' | 'error' | 'clear';
+    id: number;
+}
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NotificationsService {
+    private messages: Subject<Command>;
+    public messages$: Observable<Command>;
+    private counter = 0;
+    constructor() {
+        this.messages = new Subject<Command>();
+        this.messages$ = this.messages.asObservable();
+    }
 
-  constructor() { }
+    addSuccess(message: string) {
+        const id = this.counter++;
+        this.messages.next({
+            message,
+            type: 'success',
+            id
+        });
+        this.autoClear(id)
+    }
+    addError(message: string) {
+        const id = this.counter++;
+        this.messages.next({
+            message,
+            id,
+            type: 'error'
+        });
+        this.autoClear(id);
+    }
+    clearMessages(id: number) {
+        this.messages.next({
+            type: 'clear',
+            id
+        });
+    }
+
+    getMessages() {
+        return this.messages$.pipe(
+            scan((acc: Command[], value: Command) => {
+                if (value.type == 'error' || value.type == 'success') {
+                    return acc.concat(value);
+                } else if (value.type == 'clear') {
+                    return acc.filter((item) => item.id != value.id);
+                }
+            }, [])
+        );
+    }
+
+    autoClear(id: number) {
+        setTimeout(() => {
+            this.clearMessages(id);
+        }, 5000);
+    }
 }
